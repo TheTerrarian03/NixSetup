3908fc9a050484352f94a687979a9ec8c5ec9a29
Logan Meyers <theterrarian03@gmail.com>
2025-03-25 20:08:52 -0700

    update config file and autostart for openbox

--- config.sh
+++ config.sh
@@ -1,8 +1,14 @@
+# copy files
 sudo cp resources/configuration.nix /etc/nixos/configuration.nix
 
 mkdir -p ~/.config/
-cp -r resources/.config ~/.config/
+cp -r resources/.config/ ~/.config/
 
 cp resources/WALLPAPER.png ~/.config
 
+# rebuild nixos
 sudo nixos-rebuild switch
+
+# add zen browser
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub app.zen_browser.zen
